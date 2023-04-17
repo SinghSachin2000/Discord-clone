ae72c04105be1a7e5850aad095c8fbf0f28abe84
Sachin <rahulit1708@gmail.com>
2023-04-17 16:16:16 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontfamily:{
+    whitney:["whitney"],
+    GintoNord:["GintoNord"],
+    Ginto:["Ginto"],
+    ggsans:["ggsans"]
+  }
+}
